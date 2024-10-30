bbdf3342e12ab21c91d25ed7f9629a12648dcd49
JChoi2527 <JChoi2527@gmail.com>
2024-10-30 17:34:29 +0900

    various improvements spacing is consistent between first card and non-first cards as well as quitting program via exit input or ctrl+c score is always displayed at top

--- src/main.py
+++ src/main.py
@@ -4,8 +4,14 @@ import json
 from pathlib import Path
 import random
 from collections import deque
+from enum import Enum
 
-def clear():
+class Result(Enum):
+    EXIT = 0
+    CORRECT = 1
+    INCORRECT = -1
+
+def clear_cli():
     # windows
     if name == 'nt':
         _ = system('cls')
@@ -15,7 +21,6 @@ def clear():
         _ = system('clear')
 
 recent_queue = []
-
 def biased_shuffle (n):
     while True:
         random_element = random.choice(n)
@@ -25,59 +30,76 @@ def biased_shuffle (n):
                 recent_queue.pop(0)
             return random_element
 
-path = Path(__file__).parent / "../json/cards.json"
-with path.open() as file:
-    data = json.load(file)
-
-cards = data["cards"]
-
-correct_count = 0
-total_count = 0
-
-incorrect_cards = []
-
-clear()
-
-try:
-    while True:
-        # Select a random word entry
-        random_card = biased_shuffle(cards)
-
-        # Print the word
-        print(random_card["front"])
-
-        # Prompt the user for input and save it in a variable
-        user_input = input("Input: ")
-
-        clear()
-
-        if user_input == random_card["back"]:
-            correct_count += 1
-            total_count += 1
-            print("Correct!")
-            print("")
-
-        elif user_input == "exit":
-            print("Score: " + str(correct_count) + "/" + str(total_count))
-            if incorrect_cards:
-                print("Incorrect list:")
-                for card in incorrect_cards:
-                    print(card["front"] + ": " + card["back"])
-            sys.exit()
-
-        else:
-            if not random_card in incorrect_cards:
-                incorrect_cards.append(random_card)
-            total_count += 1
-            print("INCORRECT, answer is:")
-            print(random_card["back"])
-            print("")
-except KeyboardInterrupt:
-    print("")
-    print("")
-    print("Score: " + str(correct_count) + "/" + str(total_count))
-    if incorrect_cards:
+def print_incorrect(incorrect_list):
+    if incorrect_list:
         print("Incorrect list:")
-        for card in incorrect_cards:
+        for card in incorrect_list:
             print(card["front"] + ": " + card["back"])
-    exit(0)
+        print("")
+
+def main():
+    path = Path(__file__).parent / "../json/cards.json"
+    with path.open() as file:
+        data = json.load(file)
+
+    cards = data["cards"]
+
+    correct_count = 0
+    total_count = 0
+
+    incorrect_cards = []
+
+    clear_cli()
+
+    print("Ankikado")
+    print("")
+    print("")
+    print("")
+
+    try:
+        while True:
+            # Select a random word entry
+            random_card = biased_shuffle(cards)
+
+            # Print the word
+            print(random_card["front"])
+
+            # Prompt the user for input and save it in a variable
+            user_input = input("Input: ")
+
+            clear_cli()
+
+            if user_input == random_card["back"]:
+                result = Result.CORRECT
+                correct_count += 1
+                total_count += 1
+
+            elif user_input == "exit":
+                result = Result.EXIT
+
+            else:
+                result = Result.INCORRECT
+                if not random_card in incorrect_cards:
+                    incorrect_cards.append(random_card)
+                total_count += 1
+
+            print("Score: " + str(correct_count) + "/" + str(total_count))
+            print("")
+
+            match result:
+                case Result.CORRECT:
+                    print("Correct!")
+                    print("")
+                case Result.INCORRECT:
+                    print("INCORRECT, answer is: " + random_card["back"])
+                    print("")
+                case Result.EXIT:
+                    print_incorrect(incorrect_cards)
+                    sys.exit()
+
+    except KeyboardInterrupt:
+        clear_cli()
+        print("Score: " + str(correct_count) + "/" + str(total_count))
+        print("")
+        print_incorrect(incorrect_cards)
+        exit(0)
