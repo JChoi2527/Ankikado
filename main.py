0e61ca7f2736396c1774813547313696c1b67508
Jake Choi <JChoi2527@gmail.com>
2025-09-25 19:14:33 +0900

    leitner method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import random
+import math
 from os import system, name
 from pathlib import Path
 from enum import Enum
@@ -20,76 +21,82 @@ def main():
     with path.open(encoding='utf-8') as file:
         data = json.load(file)
 
-    all_cards = data["cards"]
+    cards = data["cards"]
+    cards_num = len(cards)
+    boxes_num = 3
+    boxes = [[] for _ in range(boxes_num)]
+    boxes[0] = cards
+    boxes_queue = [[] for _ in range(boxes_num)]
+    
+    boxes_order = []
+    increment = 1
+    for box_index in range(boxes_num - 1, -1, -1):
+        for i in range(math.ceil(cards_num * (0.5 ** (boxes_num - increment)))):
+            boxes_order.append(box_index)
+        increment += 1
+
     incorrect_cards = []
 
     correct_count = 0
     total_count = 0
-
-    all_queue = []
-    incorrect_queue = []
 
     clear_cli()
 
     print("Ankikado")
     print("")
     print(str(args.deck))
-    print(str(len(all_cards)) + " cards")
+    print(str(len(cards)) + " cards")
     print("")
 
     try:
+        order_index = 0
         while True:
-            # every n cards, select random from incorrect_queue if it exists
-            if len(incorrect_cards) > 0:
-                match len(incorrect_cards):
-                    case 1:
-                        incorrect_period = 20
-                    case 2:
-                        incorrect_period = 15
-                    case 3:
-                        incorrect_period = 11
-                    case 4:
-                        incorrect_period = 8
-                    case 5:
-                        incorrect_period = 6
-                    case _:
-                        incorrect_period = 5
-
-                if total_count % incorrect_period == 0:
-                    random_card = biased_shuffle(incorrect_cards, incorrect_queue)
-                else:
-                    random_card = biased_shuffle(all_cards, all_queue)
-            else:
-                random_card = biased_shuffle(all_cards, all_queue)
+            selected_card = False
+            while not selected_card:
+                while len(boxes[boxes_order[order_index]]) <= 0:
+                    order_index += 1
+                    if order_index >= len(boxes_order):
+                        order_index = 0
+                selected_card = biased_shuffle(boxes[boxes_order[order_index]], boxes_queue[boxes_order[order_index]])
+                if not selected_card:
+                    order_index += 1
+                    if order_index >= len(boxes_order):
+                        order_index = 0
 
             # print word
-            print(random_card["front"])
+            print(selected_card["front"])
 
             # prompt user for input and save it in a variable
             user_input = input("Answer: ")
 
             clear_cli()
 
-            if user_input == random_card["back"]:
+            if user_input == selected_card["back"]:
                 result = Result.CORRECT
+                if boxes_order[order_index] + 1 < boxes_num:
+                    boxes[boxes_order[order_index]].remove(selected_card)
+                    boxes_queue[boxes_order[order_index]].remove(selected_card)
+                    boxes[boxes_order[order_index] + 1].append(selected_card)
+                    boxes_queue[boxes_order[order_index] + 1].append(selected_card)
                 correct_count += 1
                 total_count += 1
             elif user_input == "exit":
                 result = Result.EXIT
             else:
                 result = Result.INCORRECT
-                if not random_card in incorrect_cards:
-                    incorrect_cards.append(random_card)
-                # for card in all_cards:
-                #     if user_input == card["back"]:
-                #         if not card in incorrect_cards:
-                #             incorrect_cards.append(card)
-                #         break
+                if not selected_card in incorrect_cards:
+                    incorrect_cards.append(selected_card)
+                if boxes_order[order_index] != 0:
+                    boxes[boxes_order[order_index]].remove(selected_card)
+                    boxes_queue[boxes_order[order_index]].remove(selected_card)
+                    boxes[0].append(selected_card)
+                    boxes_queue[0].append(selected_card)
                 total_count += 1
 
             print("Score: " + str(correct_count) + "/" + str(total_count))
-            if (len(all_queue) == 0):
-                print("Deck reset")
+
+            if len(boxes[boxes_num - 1]) == cards_num:
+                print("All cards in final box!!!")
             else:
                 print("")
 
@@ -100,11 +107,19 @@ def main():
                     print("")
                 case Result.INCORRECT:
                     print("INCORRECT")
-                    print(random_card["front"] + ": " + random_card["back"])
+                    print(selected_card["front"] + ": " + selected_card["back"])
                     print("")
                 case Result.EXIT:
                     print_incorrect(incorrect_cards)
                     sys.exit()
+            
+            for i in range(boxes_num):
+                if (len(boxes_queue[i]) >= len(boxes[i])):
+                    boxes_queue[i].clear()
+            
+            order_index += 1
+            if order_index >= len(boxes_order):
+                order_index = 0
 
     except KeyboardInterrupt:
         clear_cli()
@@ -118,27 +133,26 @@ class Result(Enum):
     CORRECT = 1
     INCORRECT = -1
 
-def biased_shuffle (cards, queue):
-    if not cards:
+def biased_shuffle (list, queue):
+    # if list is empty, return false
+    if not list:
         return False
     
-    # create list of cards not in queue
-    not_in_queue = cards.copy()
+    # create list of elements not in queue
+    not_in_queue = list.copy()
     for card in queue:
-        # if card in not_in_queue:
-        not_in_queue.remove(card)
+        if card in not_in_queue:
+            not_in_queue.remove(card)
 
-    # if all cards are in queue, return false
+    # if all elements are in queue, return false
     if not not_in_queue:
         return False
     
-    random_card = random.choice(not_in_queue)
+    random_selection = random.choice(not_in_queue)
 
-    # add selected card to queue, clear queue if all cards in queue
-    queue.append(random_card)
-    if len(queue) >= len(cards):
-        queue.clear()
-    return random_card
+    # add selected card to queue
+    queue.append(random_selection)
+    return random_selection
 
 def print_incorrect(incorrect_list):
     if incorrect_list:
